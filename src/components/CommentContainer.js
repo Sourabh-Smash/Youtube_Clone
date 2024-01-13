92f77e9bbd23925018a6c33403380c16f01b59b2
Sourabh Manawat <souramena123@gmail.com>
2024-01-13 19:33:34 +0530

    made YT nested comment section

---
+++ src/components/CommentContainer.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+
+const commentsObj = [
+  {
+    name: "Sourabh",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+    replies: [],
+  },
+  {
+    name: "Sourabh",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+    replies: [
+      {
+        name: "Sourabh",
+        text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+        replies: [],
+      },
+      {
+        name: "Sourabh",
+        text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+        replies: [
+          {
+            name: "Sourabh",
+            text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+            replies: [
+              {
+                name: "Sourabh",
+                text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+                replies: [],
+              },
+              {
+                name: "Sourabh",
+                text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+                replies: [],
+              },
+            ],
+          },
+          {
+            name: "Sourabh",
+            text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+            replies: [],
+          },
+          {
+            name: "Sourabh",
+            text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+            replies: [],
+          },
+        ],
+      },
+      {
+        name: "Sourabh",
+        text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Sourabh",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+    replies: [],
+  },
+  {
+    name: "Sourabh",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+    replies: [],
+  },
+  {
+    name: "Sourabh",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, suscipit.",
+    replies: [],
+  },
+];
+
+const CommentBox = ({ data }) => {
+  const { name, text, replies } = data;
+  return (
+    <div className="flex items-center m-2 bg-gray-600 px-4 py-2 rounded-lg shadow-md shadow-gray-400 mb-3 ">
+      <FaUserCircle className="text-white h-9 w-8" />
+      <div className="mx-2">
+        <h1 className="font-semibold">{name}</h1>
+        <p className="text-xs">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentList = ({ eachComment }) => {
+  return eachComment.map((comment, idx) => (
+    <div key={idx} className="">
+      <CommentBox data={comment} />
+      <div className="ml-5 border-l-2  border-l-slate-500">
+        <CommentList eachComment={comment?.replies} />
+      </div> 
+    </div>
+  ));
+};
+
+const CommentContainer = () => {
+  return (
+    <>
+      <div className="m-3">
+        <div className="text-2xl font-semibold">Commnet Section</div>
+        <CommentList eachComment={commentsObj} />
+      </div>
+    </>
+  );
+};
+
+export default CommentContainer;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { removeMenu } from "../utils/homeVideoSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentContainer from "./CommentContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -11,16 +12,22 @@ const WatchPage = () => {
     dispach(removeMenu());
   }, []);
   return (
-    <div className="px-5">
-      <iframe
-        width="600"
-        height="315"
-        src={"https://www.youtube.com/embed/"+searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div
+      className="
+    "
+    >
+      <div className="px-5">
+        <iframe
+          width="600"
+          height="315"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <CommentContainer/>
     </div>
   );
 };
